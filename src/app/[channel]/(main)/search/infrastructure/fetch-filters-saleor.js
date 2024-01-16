b738d7f6cb63e17518882d4af8463dad5a3022d9
Wojtek Oksiński <wojciech.oksinski@mirumee.com>
2024-01-16 14:36:47 +0100

    search and filters prototype

--- src/app/[channel]/(main)/search/infrastructure/fetch-filters-saleor.js
+++ src/app/[channel]/(main)/search/infrastructure/fetch-filters-saleor.js
@@ -1,51 +1,46 @@
 import { AttributesListDocument } from "@/gql/graphql";
 import { executeGraphQL } from "@/lib/graphql";
 
-const getSearchInfo = ({
-    pageInfo,
-    totalCount
-}) => ({
-    totalCount,
-    hasNextPage: pageInfo.hasNextPage,
-    nextPageParams: { cursor: pageInfo.endCursor }
-})
-
-
 export const fetchFiltersSaleorInfra = async ({
-    searchParams,
-    // variables,
-    revalidate
+	searchParams,
+	// variables,
+	revalidate,
 }) => {
-    try {
-        // const cursor = typeof searchParams.cursor === "string" ? searchParams.cursor : null;
-        // name  products doenst seem to fully coresspond with the content
-        const { attributes } = await executeGraphQL(AttributesListDocument, {
-            // variables: {
-            //     ...variables,
-            //     after: cursor,
-            // },
-            revalidate
-        });
-        const filterCategoriesMap = attributes.edges
-            .reduce((categoryAcc, edge) => ({
-                ...categoryAcc,
-                [edge.node.name]: edge.node.choices.edges.reduce((filterAcc, edge) => ({
-                    ...filterAcc,
-                    [edge.node.name]: null
-                }), {})
-            }), {})
+	try {
+		// const cursor = typeof searchParams.cursor === "string" ? searchParams.cursor : null;
+		// name  products doenst seem to fully coresspond with the content
+		const { attributes } = await executeGraphQL(AttributesListDocument, {
+			// variables: {
+			//     ...variables,
+			//     after: cursor,
+			// },
+			revalidate,
+		});
+		const filterCategoriesMap = attributes.edges.reduce(
+			(categoryAcc, edge) => ({
+				...categoryAcc,
+				[edge.node.name]: edge.node.choices.edges.reduce(
+					(filterAcc, edge) => ({
+						...filterAcc,
+						[edge.node.name]: null,
+					}),
+					{},
+				),
+			}),
+			{},
+		);
 
-        // console.log('filterCategoriesMap', filterCategoriesMap)
-        return {
-            isSuccess: true,
-            filterCategoriesMap
-            // products: products.edges.map((edge) => edge.node),
-            // searchInfo: getSearchInfo(products)
-        }
-    } catch (error) {
-        return {
-            isSuccess: false,
-            error
-        }
-    }
-}
+		// console.log('filterCategoriesMap', filterCategoriesMap)
+		return {
+			isSuccess: true,
+			filterCategoriesMap,
+			// products: products.edges.map((edge) => edge.node),
+			// searchInfo: getSearchInfo(products)
+		};
+	} catch (error) {
+		return {
+			isSuccess: false,
+			error,
+		};
+	}
+};
